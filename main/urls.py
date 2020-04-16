33c4dd8a635726d16f9a08908f3e7002825c094e
captainvlad <zadorozhnyy@ucu.edu.ua>
2020-04-16 15:01:48 +0300

    Second commit

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     url('main', views.respond_1, name = "index"),
-    url('home_2/home_3/?', views.respond_3, name = "index"),
-    url('home_2/', views.respond_2, name = "index"),
-    url('home_4/', views.respond_4, name = "index"),
-    url('home_5/', views.respond_5, name = "index"),
+    url('home_3', views.respond_3, name = "index"),
+    url('home_2', views.respond_2, name = "index"),
+    url('home_4', views.respond_4, name = "index"),
+    url('home_5', views.respond_5, name = "index"),
     url('home_6', views.respond_7, name = "index"),
     url('home_7', views.respond_6, name = "index"),
     url('home_9', views.respond_8, name = "index"),
