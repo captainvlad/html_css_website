997845866499b2ea5a009b87730bda0d31951e61
captainvlad <zadorozhnyy@ucu.edu.ua>
2020-04-16 14:24:28 +0300

    Initial commit

---
+++ main/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url('main', views.respond_1, name = "index"),
+    url('home_2/home_3/?', views.respond_3, name = "index"),
+    url('home_2/', views.respond_2, name = "index"),
+    url('home_4/', views.respond_4, name = "index"),
+    url('home_5/', views.respond_5, name = "index"),
+    url('home_6', views.respond_7, name = "index"),
+    url('home_7', views.respond_6, name = "index"),
+    url('home_9', views.respond_8, name = "index"),
+    url('home_10', views.respond_9, name = "index"),
+    url('home_11', views.respond_10, name = "index"),
+    url('home_12', views.respond_11, name = "index"),
+]
---
+++ main/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def respond_1(request):
+    return render(request, 'main/home.html')
+
+def respond_2(request):
+    return render(request, 'main/home_2.html')
+
+def respond_3(request):
+    return render(request, 'main/home_3.html')
+
+def respond_4(request):
+    return render(request, 'main/home_4.html')
+
+def respond_5(request):
+    return render(request, 'main/home_5.html')
+
+def respond_6(request):
+    return render(request, 'main/home_6.html')
+
+def respond_7(request):
+    return render(request, 'main/home_7.html')
+
+def respond_8(request):
+    return render(request, 'main/home_9.html')
+
+def respond_9(request):
+    return render(request, 'main/home_10.html')
+
+def respond_10(request):
+    return render(request, 'main/home_11.html')
+
+def respond_11(request):
+    return render(request, 'main/home_12.html')
